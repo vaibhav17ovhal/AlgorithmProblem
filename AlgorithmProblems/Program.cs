70302e7d95a49459cd38f52134ac736a1dfd2ae4
vaibhav17ovhal <125484163+vaibhav17ovhal@users.noreply.github.com>
2023-05-19 18:41:10 +0530

    Update Program.cs

--- AlgorithmProblems/Program.cs
+++ AlgorithmProblems/Program.cs
@@ -8,23 +8,6 @@ namespace AlgorithmProblems
         {
             Console.WriteLine("Welcome to Algorithm Problem");
 
-            //Console.WriteLine("\n ===================== \n");
-
-            //Problems pp = new Problems();
-            //string a = "VAI";
-            //int n = a.Length;
-            //pp.Permutation(a , 0, n - 1);
-
-            //Console.WriteLine("\n ===================== \n");
-
-            //BinarySearch bs = new BinarySearch();
-            //bs.Binary();
-
-            //Console.WriteLine("\n ===================== \n");
-
-            //InsertionSort insertion = new InsertionSort();
-            //insertion.Sort();
-
             Console.WriteLine("\n ===================== \n");
 
             BubbleSort bubble = new BubbleSort();
